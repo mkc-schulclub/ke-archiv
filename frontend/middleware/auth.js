010e4db94071ecdd74c2f8955330a51099d1f251
probablyjassin <aouanijassin@gmail.com>
2023-10-03 15:41:22 +0200

    idk if this is goof

--- frontend/middleware/auth.js
+++ frontend/middleware/auth.js
@@ -1,8 +1,9 @@
 const session = useCookie('session')
+const router = useRouter()
 
 export default defineNuxtRouteMiddleware((to, from) => {
   if (to.path == '/admin' && !session.value) {
-    console.log("You're not logged in, redirecting")
-    return navigateTo('/')
+    router.push('/')
+    return 
   }
 })
