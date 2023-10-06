5e79a9417f6eccaa6e4743c70d05979cdfdcdf31
probablyjassin <aouanijassin@gmail.com>
2023-10-06 17:32:41 +0200

    working on admin/index

--- frontend/pages/admin/index.vue
+++ frontend/pages/admin/index.vue
@@ -1,12 +1,15 @@
 <template>
   <transition>
     <div>
-    
+    <h2>Anzahl von Nutzern: {{ users.length }}</h2>
+    <h2>Anzahl von Ausgaben: {{ ausgaben.length }}</h2>
   </div>  
   </transition>
 </template>
 
   <script setup>
+  const { ausgaben } = useAusgaben()
+  const { users } = useUsers()
   definePageMeta({
     layout: "admin",
   });
