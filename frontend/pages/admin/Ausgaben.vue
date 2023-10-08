8fef651ffade3b42abbc10f54922412d314dc731
probablyjassin <aouanijassin@gmail.com>
2023-10-08 11:32:59 +0200

    testing: btn to refetch manually

--- frontend/pages/admin/Ausgaben.vue
+++ frontend/pages/admin/Ausgaben.vue
@@ -1,28 +1,28 @@
 <template>
-    <transition>
-        <div>
-            <div class="container">
-                <h1>Liste aller Ausgaben 
-                    <a href="#" class="fas fa-plus gray-plus-icon" style="font-size: medium; text-decoration: none;" @click="mode = 'add'"></a>
-                </h1>
-    </div>
-            <div v-for="ausgabe in ausgaben">
-                {{ ausgabe.title }}
-                <div class="divider"></div>
-            </div>
-            <AdminAdd v-if="mode == 'add'" @exit="mode = 'view'"/>
-        </div>
-    </transition>
+	<transition>
+		<div>
+			<div class="container">
+				<h1>
+					Liste aller Ausgaben
+					<a href="#" class="fas fa-plus gray-plus-icon" style="font-size: medium; text-decoration: none" @click="mode = 'add'"></a>
+				</h1>
+			</div>
+			<div v-for="ausgabe in ausgaben">
+				{{ ausgabe.title }}
+				<div class="divider"></div>
+			</div>
+            <button class="btn btn-primary" @click.prevent="fetchAusgaben">Refetch</button>
+			<AdminAdd v-if="mode == 'add'" @exit="mode = 'view'" />
+		</div>
+	</transition>
 </template>
 
 <script setup>
-const { ausgaben } = useAusgaben()
-definePageMeta({
-    layout: "admin",
-});
-let mode = ref("view")
+	const { ausgaben, fetchAusgaben } = useAusgaben();
+	definePageMeta({
+		layout: "admin",
+	});
+	let mode = ref("view");
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
